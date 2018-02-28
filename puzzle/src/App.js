3d600b30b74547f842c637b7d2bcbca56106bf23
Jirayu Limjinda <jir4yu.l@gmail.com>
2018-02-28 20:18:34 +0700

    update for heroku

--- puzzle/src/App.js
+++ puzzle/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
+import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom"
 import './App.css'
 
 import QuizTemplate from './components/QuizTemplate'
@@ -15,38 +15,40 @@ class App extends Component {
               <NavLink activeClassName="active" to="/quiz2">Quiz 2</NavLink>
               <NavLink activeClassName="active" to="/quiz3">Quiz 3</NavLink>
             </div>*/}
-            <Route exact 
-              path="/" 
-              render={() => 
-                <QuizTemplate 
-                  quizNumber={1} 
-                  text="Guess who's in the picture?" 
-                  sliceNumber={9} 
-                /> 
-              } 
-            />
-            {/* Quiz 2 */}
-            <Route exact 
-              path="/quiz2" 
-              render={() => 
-                <QuizTemplate 
-                  quizNumber={2} 
-                  text="to easy, how about this?" 
-                  sliceNumber={12} 
-                /> 
-              } 
-            />
-            {/* Quiz 3 */}
-            <Route exact 
-              path="/quiz3" 
-              render={() => 
-                <QuizTemplate 
-                  quizNumber={3} 
-                  text="If you guess them right, you're insane!" 
-                  sliceNumber={25} 
-                /> 
-              } 
-            />
+            <Switch>
+              <Route exact 
+                path="/" 
+                render={() => 
+                  <QuizTemplate 
+                    quizNumber={1} 
+                    text="Guess who's in the picture?" 
+                    sliceNumber={9} 
+                  /> 
+                } 
+              />
+              {/* Quiz 2 */}
+              <Route 
+                path="/quiz2" 
+                render={() => 
+                  <QuizTemplate 
+                    quizNumber={2} 
+                    text="to easy, how about this?" 
+                    sliceNumber={12} 
+                  /> 
+                } 
+              />
+              {/* Quiz 3 */}
+              <Route 
+                path="/quiz3" 
+                render={() => 
+                  <QuizTemplate 
+                    quizNumber={3} 
+                    text="If you guess them right, you're insane!" 
+                    sliceNumber={25} 
+                  /> 
+                } 
+              />
+            </Switch>
           </div>
         </div>
       </Router>
